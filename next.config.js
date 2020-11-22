33b6d199c0a026917c370a1e1db18e48aacde772
Gaurav Tewari <gauravtewari111@gmail.com>
2020-11-22 21:28:35 +0530

    config changed

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   images: {
-    domains: ["unsplash.com", "images.unsplash.com", "gravatar.com"],
+    domains: [
+      "unsplash.com",
+      "images.unsplash.com",
+      "gravatar.com",
+      "www.gravatar.com",
+      "images.gravatar.com",
+    ],
   },
 };
