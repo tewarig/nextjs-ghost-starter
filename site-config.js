eda9c40d691027dcb35157b7ef8a2d2326eefc04
Gaurav Tewari <gauravtewari111@gmail.com>
2021-02-18 20:33:22 +0530

    Update site-config.js

--- site-config.js
+++ site-config.js
@@ -1,5 +1,5 @@
 //your site url here..
-const appUrl = "https://next-ghost-blog.herokuapp.com/";
-const contentKey = "382362c8a9a611d23db0adefc1";
-export { appUrl, contentKey };
+const appUrl = "https://animegeeks-blog.herokuapp.com/";
+const contentKey = "3d392dd5eed4123dbb01318aa7";
+export { appUrl, contentKey }
 //https://onepbetter.herokuapp.com/ghost/api/v3/content/posts/?key=413feaabea32862b596edb8ee1
